27d799be1305b9330d4e6cb1ed40cbc377abe201
Oussama Dhahri <oussamadhahri78@gmail.com>
2025-01-06 02:15:19 +0100

    DEV:Adjusting back office view and SSR logic

--- cars/urls.py
+++ cars/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from cars.views import CarViewSet
-
+app_name = 'cars'
 urlpatterns = [
     path('create/', CarViewSet.as_view({'post': 'create_car'})),
     path('list/', CarViewSet.as_view({'get': 'list_cars'})),
     path('get/<int:pk>/', CarViewSet.as_view({'get': 'get_car'})),
     path('update/<int:pk>/', CarViewSet.as_view({'put': 'update_car'})),
-    path('delete/<int:pk>/', CarViewSet.as_view({'delete': 'delete_car'})),
+    path('delete/<int:pk>/', CarViewSet.as_view({'delete': 'delete_car'}),name='delete_car'),
     path('get_by_name/<str:name>/', CarViewSet.as_view({'get': 'get_car_by_name'})),
     path('car_list/', CarViewSet.as_view({'get': 'car_list_page'}),name='car_list_page'),
     path('car_detail/<int:pk>/', CarViewSet.as_view({'get': 'car_detail_page'}),name='car_detail_page'),
--- office/views.py
+++ office/views.py
@@ -4,8 +4,11 @@ from django.shortcuts import render
 
 from rest_framework.viewsets import GenericViewSet
 
+from cars.serializers.car_view_serializer import CarSerializer
+from cars.services.car_service import CarService
+
 class OfficeViewSet(GenericViewSet):
-    
+    car_service = CarService()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -14,22 +17,21 @@ class OfficeViewSet(GenericViewSet):
         return render(request, 'office_default_page.html')
 
     def show_office_cars_page(self, request):
+        cars_list = self.car_service.list()
+        serializer = CarSerializer()
+        columns = [field for field in serializer.fields]
+
         context = {
             'table_title': 'Cars Management',
-            'columns': [
-                {'key': 'name', 'label': 'Car Name'},
-                {'key': 'model', 'label': 'Model'},
-                {'key': 'year', 'label': 'Year'},
-                {'key': 'status', 'label': 'Status'},
-                {'key': 'price', 'label': 'Price'},
-            ],
-            'items': [],  # Add your cars queryset
+            'columns': columns,  # Add your car fields
+            'items': cars_list,  # Add your cars queryset
             'show_actions': True,
             'show_add_button': True,
             'add_url': 'add_car',  # URL name for adding a car
             'view_url': 'car_detail',  # URL name for viewing a car
             'edit_url': 'edit_car',  # URL name for editing a car
         }
+        print('carss columns is :',context['items']) 
         return render(request, 'cars.html', context)
 
     def show_office_agents_page(self, request):
